48671301b5a605ff3830d8530455f8e877aed5e8
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-08-09 08:54:06 +0200

    first commit

---
+++ utils/animation.js
@@ -0,0 +1,62 @@
+export const containerVariants = (delay = 0)=> ({
+    "offscreen": {
+        opacity: 0,
+        y: 30
+    },
+    "onscreen": {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            duration: 2,
+            delay
+        }
+    }
+})
+
+export const tagVariants = {
+    "offscreen": {
+        opacity: 0,
+        y: 10
+    },
+    "onscreen": {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            duration: 2.8,
+            delay: .4
+        }
+    }
+}
+
+export const titleVariants = {
+    "offscreen": {
+        opacity: 0,
+        y: 30
+    },
+    "onscreen": {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            duration: 2.2,
+        }
+    }
+}
+
+export const desVariants = {
+    "offscreen": {
+        opacity: 0,
+        y: 20
+    },
+    "onscreen": {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            duration: 2.6,
+            delay:.2
+        }
+    }
+}
