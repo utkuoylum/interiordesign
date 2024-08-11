5b7c15220eb363df1341e965070f810bb14a470e
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-08-11 20:47:37 +0200

    contact bg fix

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -18,7 +18,7 @@ const page = () => {
   const scale = useTransform(scrollYProgress, [0, 1], [.6, 1])
   return (
     <div>
-      <div className="bg-[url('/image/whoweare.jpg')] bg-center bg-cover">
+      <div className="bg-[url('/image/whoweare.jpg')] bg-center bg-cover bg-fixed">
         <h1 className='container py-32 text-6xl font-semibold text-black tracking-widest text-center lg:py-64'>Who are we?</h1>
       </div>
 
